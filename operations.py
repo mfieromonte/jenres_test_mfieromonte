036e921cdb2822bff69256df178ea8ab9894f404
mfieromonte <77149506+mfieromonte@users.noreply.github.com>
2024-02-13 19:06:24 +0100

    Update operations.py

--- operations.py
+++ operations.py
@@ -1,4 +1,10 @@
 def get_sums(nums):
   sums = sum(nums)
   unused = ''
+  string = 'hello world'
+  string = 'hello world'
+  string = 'hello world'
+  string = 'hello world'
+  string = 'hello world'
+  string = 'hello world'
   return sums 
