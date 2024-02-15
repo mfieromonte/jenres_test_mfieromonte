32361e340e3577a7c2e4d4b5fd6bee5693db9a99
mfieromonte <77149506+mfieromonte@users.noreply.github.com>
2024-02-15 13:49:47 +0100

    Update operations.py for hk

--- operations.py
+++ operations.py
@@ -1,4 +1,9 @@
 def get_sums(nums):
   sums = sum(nums)
   unused = ''
+  hello = 'hello world'
+  hello = 'hello world'
+  hello = 'hello world'
+  hello = 'hello world'
+  hello = 'hello world'
   return sums 
