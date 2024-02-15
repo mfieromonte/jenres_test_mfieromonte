a4eb683e966f829bbc41c0757aee0e4b1ba8c1aa
Scrooj <scrooj@res-it.com>
2024-02-15 12:50:25 +0000

    "Remove unused variables to comply with SonarQube rule python:S1481"
    This commit message succinctly describes the action taken (removal of unused variables) and the reason for the change (compliance with a specific SonarQube rule).
    
    Scrooj commit tags:
    sonar_issue_key:AY2s0RUYbvf-BhyM4vYl

--- operations.py
+++ operations.py
@@ -1,9 +1,3 @@
 def get_sums(nums):
   sums = sum(nums)
-  unused = ''
-  hello = 'hello world'
-  hello = 'hello world'
-  hello = 'hello world'
-  hello = 'hello world'
-  hello = 'hello world'
   return sums 
