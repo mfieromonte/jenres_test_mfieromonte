e701e8ea7cbd289db47936ad8615a4a42e7cc13d
mfieromonte <77149506+mfieromonte@users.noreply.github.com>
2024-02-20 11:09:21 +0100

    Update main.py

--- my_python_project/src/main.py
+++ my_python_project/src/main.py
@@ -1,6 +1,12 @@
 # This is the main application file.
 
 def main():
+    str = 'abc'
+    str = 'abc'
+    str = 'abc'
+    str = 'abc'
+    str = 'abc'
+    str = 'abc'
     print("Hello, World!")
 
 if __name__ == "__main__":
