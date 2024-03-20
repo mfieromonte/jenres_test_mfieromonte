70cc13013ce3aacd5f017b5754eb88d804d3f56f
mfieromonte <martina.fieromonte@res-it.com>
2024-03-20 09:32:05 +0100

    deletion

--- my_python_project/tests/test_main.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from src.main import main
-
-class TestMain(unittest.TestCase):
-    def test_main_output(self):
-        self.assertEqual(main(), None)  # Assuming main function returns None
-
-if __name__ == '__main__':
-    unittest.main()
