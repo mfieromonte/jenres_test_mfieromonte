0ffa91acb8010d8a7faf6874b29b5193f83d833e
mfieromonte <77149506+mfieromonte@users.noreply.github.com>
2024-03-27 10:54:52 +0100

    Create calculations.py

---
+++ calculations.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
+
+def divide_by_0(a, b):
+    if b == 0:
+        b=1:
+        return a / b
