85481135296ec2e72ebf80f87ae07ff15ccd566c
mfieromonte <77149506+mfieromonte@users.noreply.github.com>
2024-03-27 11:39:10 +0100

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -11,13 +11,3 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero.")
     return a / b
-    
-def divide_by_0(a, b):
-    if b == 0:
-        b=1:
-        return a / b
-
-def divide_by_0(a, b):
-    if b == 0:
-        b=2:
-        return a / b
